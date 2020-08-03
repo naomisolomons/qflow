f85323d4f07da82efc86d5a0b93c24805b4a1e5b
Sam Mister <sm15883@bristol.ac.uk>
2020-08-04 00:26:10 +0100

    Mixing is now animated.

--- Intro_1_data.js
+++ Intro_1_data.js
@@ -1,6 +1,11 @@
 function mix (){
-    edgeArray[0].changeweight(-0.5)
-    edgeArray[1].changeweight(0.5)
-    edgeArray[2].changeweight(0.5)
-    edgeArray[3].changeweight(-0.5)
+  for(var i = 0; i < 25 ; i++){
+    setTimeout(function(){
+      edgeArray[0].changeweight(-0.02)
+      edgeArray[1].changeweight(0.02)
+      edgeArray[2].changeweight(0.02)
+      edgeArray[3].changeweight(-0.02)
+}, i*200);
+
+}
 }
